f0f0c9f05b0c37c294cd5314ea1c3c883b4f0335
FrdCHK <43998364+FrdCHK@users.noreply.github.com>
2024-09-11 17:11:11 +0800

    unit conversion bug fix

--- tool/predict.py
+++ tool/predict.py
@@ -19,13 +19,12 @@ def predict(data, predict_time):
     for i in range(vectors.shape[0]):
         t, p = tool.cartesian_to_spherical(vectors[i, 0], vectors[i, 1], vectors[i, 2])
         # convert phi to sin & cos to avoid jump close to 2pi
-        sin_phi = np.sin(np.radians(p))
-        cos_phi = np.cos(np.radians(p))
+        sin_phi = np.sin(p)
+        cos_phi = np.cos(p)
         sphere.append([t, sin_phi, cos_phi])
     sphere = np.array(sphere)
 
     spline_t = UnivariateSpline(time_series, sphere[:, 0], k=1, ext=0)
-    # spline_p = UnivariateSpline(time_series, sphere[:, 1], k=1, ext=0)
     sin_spline_p = UnivariateSpline(time_series, sphere[:, 1], k=1, ext=0)
     cos_spline_p = UnivariateSpline(time_series, sphere[:, 2], k=1, ext=0)
 
