e93f18d6f42f1c28c775214f3063751576f069be
Jingdong Zhang <zjd98@sina.cn>
2025-12-09 18:57:23 +0200

    Added AIPS task APCAL

---
+++ plugin/core/aips_task/Apcal.py
@@ -0,0 +1,31 @@
+from typing import Dict, Any
+from AIPSTask import AIPSTask
+
+from core.Plugin import Plugin
+from core.Context import Context
+
+from .run_task import run_task
+
+
+class Apcal(Plugin):
+    def __init__(self, params: Dict[str, Any]):
+        """inname, inclass, indisk, inseq, identifier must be specified"""
+        self.params = params
+        self.task = AIPSTask("APCAL")
+
+    @classmethod
+    def get_description(cls) -> str:
+        return "Task to generate a SN table containing amplitude gain calibration information from a system temperature (TY) table and a gain curve GC table."
+    
+    def run(self, context: Context) -> bool:
+        context.logger.info("Start AIPS task APCAL")
+        run_task(self.task, self.params)
+        context.get_context()["loaded_plugins"]["AipsCatalog"].add_ext(context,
+                                                                       self.params["inname"],
+                                                                       self.params["inclass"],
+                                                                       self.params["indisk"],
+                                                                       self.params["inseq"],
+                                                                       "SN",
+                                                                       ext_source=self.params["identifier"])
+        context.logger.info("AIPS task APCAL finished")        
+        return True
